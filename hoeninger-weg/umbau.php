8857d8b0cdc9cd1cd8cc95d13503e0ad7aa6dfb7
Simon Schüßler <simonschussler@Simons-Air-2.fritz.box>
2020-07-23 18:26:03 +0200

    commit

--- hoeninger-weg/umbau.php
+++ hoeninger-weg/umbau.php
@@ -22,17 +22,13 @@
             <div class="slide__right-up" style="background-image: url(imgs/03Umbau/2019-04HoeningerUmbau.jpg);"></div>
             <div class="slide__right-down" style="background-image: url(imgs/03Umbau/2019-06HoeningerUmbau.jpg);"></div>
         </div>
-
-        <div class="slide__container slide__grid slide__grid--margin">
-            <div class="slide__left" style="background-image: url(imgs/03Umbau/2019-02HoeningerUmbau.jpg);"></div> 
-            <div class="slide__right-up" style="background-image: url(imgs/03Umbau/2019-07HoeningerUmbau.jpg);"></div>
-            <div class="slide__right-down" style="background-image: url(imgs/03Umbau/2019-08HoeningerUmbau.jpg);"></div>
-        </div>
-
+        
         <div class="slide__container slide__grid_2 slide__grid--margin">
-            <div class="slide__left" style="background-image: url(imgs/03Umbau/2020-03HoeningerUmbau.jpg);"></div> 
-            <div class="slide__right-up" style="background-image: url(imgs/03Umbau/2020-05HoeningerUmbau.jpg);"></div>
+            <div class="slide__left" style="background-image: url(imgs/03Umbau/2019-03HoeningerUmbau.jpg);"></div> 
+            <div class="slide__right-up" style="background-image: url(imgs/03Umbau/2019-05HoeningerUmbau.jpg);"></div>
         </div>
+
+        <img class="responsive-img" src="imgs/03Umbau/2019-02HoeningerUmbau.jpg" alt="Bild">
 
     </main>
     <?php 
