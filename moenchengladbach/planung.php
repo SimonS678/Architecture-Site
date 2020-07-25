c850fc30743c04adfe62873bea5b5aa9aff9a9c9
Simon Schüßler <simonschussler@Simons-Air-2.fritz.box>
2020-07-25 16:11:42 +0200

    commit

---
+++ moenchengladbach/planung.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="de">
+<?php
+    $title = 'Kai Matzak Architekt 2003 Mönchengladbach';
+    include_once('../partials/head.php')
+?>
+
+<body>
+    <main>
+    <?php
+            $siteInfo = '2003 Mönchengladbach';
+            $link2 = ['planung.php','Planung', 'active'];
+            $links = [$link2];
+            include_once('../partials/header.php');
+        ?>
+
+        <img class="responsive-img no-margin" src="imgs/02Planung/2003-01Moenchengladbach.jpg" alt="Bild">
+
+        <p class="caption" ><strong>2003 Mönchengladbach</strong> 
+        <br>Städtebaulicher Entwurf
+        <br>Städtebauliche Wohnbauplanung für ein Brennpunktgebiet in Mönchengladbach als Ersatz für vorhandene Hochhäuser aus den frühen siebziger Jahren.
+        <br>Neuplanung Gewerbezentrum:
+        <br>Planung einer überdachten Geschäftsstrasse.
+        </p>
+
+        <img class="responsive-img" src="imgs/02Planung/2003-02Moenchengladbach.jpg" alt="Bild">
+        <img class="responsive-img" src="imgs/02Planung/2003-03Moenchengladbach.jpg" alt="Bild">
+        <img class="responsive-img" src="imgs/02Planung/2003-04Moenchengladbach.jpg" alt="Bild">
+        <img class="responsive-img" src="imgs/02Planung/2003-05Moenchengladbach.jpg" alt="Bild">
+        <img class="responsive-img" src="imgs/02Planung/2003-06Moenchengladbach.jpg" alt="Bild">
+        <img class="responsive-img" src="imgs/02Planung/2003-07Moenchengladbach.jpg" alt="Bild">
+        <img class="responsive-img" src="imgs/02Planung/2003-08Moenchengladbach.jpg" alt="Bild">
+        <img class="responsive-img" src="imgs/02Planung/2003-09Moenchengladbach.jpg" alt="Bild">
+        <img class="responsive-img" src="imgs/02Planung/2003-10Moenchengladbach.jpg" alt="Bild">
+        <img class="responsive-img" src="imgs/02Planung/2003-11Moenchengladbach.jpg" alt="Bild">
+
+    </main>
+    <?php 
+        include_once('../partials/footer.php');
+    ?>
+</body>
+</html>
